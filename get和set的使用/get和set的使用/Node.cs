40b2431c8db65f784c468d8125a96a0d1aec3bfb
kora-hacker <kora.hacker@qq.com>
2015-07-04 11:30:01 +0800

    get和set的使用

---
+++ get和set的使用/get和set的使用/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace get和set的使用
+{
+    class Node
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+
+    }
+}
---
+++ get和set的使用/get和set的使用/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace get和set的使用
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node a = new Node();
+            a.Name = "JiangLinWei";
+            System.Console.WriteLine(a.Name);
+        }
+    }
+}
